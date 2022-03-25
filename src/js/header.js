3936cff361974fc2851519d828b852f757c9940b
ariados22 <ariados22@gmail.com>
2022-03-25 16:59:33 +0900

    220325 금 1659

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
   const NOWON_CLASSNAME = "now-on"; 
 
+
+  // detail-slider 핸들러
   $("#detailOpen").on("click", function () {
+    $(this).toggleClass(NOWON_CLASSNAME);
     $(".detail-slider").toggleClass(NOWON_CLASSNAME);
-  })
+  });
+  $(".detail-slider .cls-btn").on("click", function () {
+    $("#detailOpen").removeClass(NOWON_CLASSNAME);
+    $(".detail-slider").removeClass(NOWON_CLASSNAME);
+  });
 })
