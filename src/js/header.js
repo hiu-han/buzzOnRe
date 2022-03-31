9b9bd48c2cd342cf5109edf2078a01be954979ed
ariados22 <ariados22@gmail.com>
2022-03-31 16:59:18 +0900

    220331 목 1659

--- src/js/header.js
+++ src/js/header.js
@@ -11,6 +11,25 @@ $(document).ready(function () {
     $("#detailOpen").removeClass(NOWON_CLASSNAME);
     $(".detail-slider").removeClass(NOWON_CLASSNAME);
   });
+  
+  $("#moDetailOpen").on("click", function () {
+    $(this).toggleClass(NOWON_CLASSNAME);
+    $(".detail-slider").toggleClass(NOWON_CLASSNAME);
+  });
+  $(".detail-slider .cls-btn").on("click", function () {
+    $("#moDetailOpen").removeClass(NOWON_CLASSNAME);
+    $(".detail-slider").removeClass(NOWON_CLASSNAME);
+  });
+  
+  $("#moChannelOpen").on("click", function () {
+    $(this).toggleClass(NOWON_CLASSNAME);
+    $(".detail-channel-area > fieldset").toggleClass(NOWON_CLASSNAME);
+  });
+  // $(".detail-slider .cls-btn").on("click", function () {
+  //   $("#detailOpen").removeClass(NOWON_CLASSNAME);
+  //   $(".detail-channel-area > fieldset").removeClass(NOWON_CLASSNAME);
+  // });
+
 
   let sT = 0;
   $(window).scroll(function (e) {
